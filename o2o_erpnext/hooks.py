4afbaf4fbc32a6629bb271a4b49ca65854644d7e
1byZero <amitkp66@live.com>
2024-11-02 10:31:46 +0530

    merge update

--- o2o_erpnext/hooks.py
+++ o2o_erpnext/hooks.py
@@ -47,7 +47,8 @@ add_to_apps_screen = [
 
 # include js in doctype views
 # doctype_js = {"doctype" : "public/js/doctype.js"}
-# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
+# doctype_list_js = {"Purchase Receipt" : "public/js/purchase_receipt_list.js"}
+
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
 
