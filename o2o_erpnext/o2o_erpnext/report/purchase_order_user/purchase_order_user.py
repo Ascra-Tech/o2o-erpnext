f3d4eeb62a54d4f808ba818bdff3aff5c1710cca
1byzero <amitkp66@live.com>
2025-04-29 18:40:34 +0530

    updated report

--- o2o_erpnext/o2o_erpnext/report/purchase_order_user/purchase_order_user.py
+++ o2o_erpnext/o2o_erpnext/report/purchase_order_user/purchase_order_user.py
@@ -13,12 +13,14 @@ def get_data(filters):
     user_roles = frappe.get_roles(frappe.session.user)
     is_supplier = "Supplier" in user_roles
     is_person_raising_request = "Person Raising Request" in user_roles
+    is_requisition_approver = "Requisition Approver" in user_roles
+    is_po_approver = "PO Approver" in user_roles
     
     # Base query with sub_branch added
     base_query = '''
         SELECT supplier,
                custom_branch,
-               custom_sub_branch,  # Added sub_branch column
+               custom_sub_branch,
                shipping_address_display,
                custom_order_code,
                name,
@@ -45,15 +47,29 @@ def get_data(filters):
         else:
             # If we can't find a supplier linked to this user, return empty data
             data = []
-    elif is_person_raising_request:
-        # Find the employee linked to this user
-        employee = frappe.db.get_value("Employee", 
-                                      {"user_id": frappe.session.user}, 
-                                      ["custom_supplier", "branch", "custom_sub_branch"],
-                                      as_dict=True)
+    elif is_person_raising_request or is_requisition_approver or is_po_approver:
+        # For Person Raising Request, Requisition Approver, and PO Approver roles
+        # use the same logic - find employee and their associated entities
+        employee_name = frappe.db.get_value("Employee", {"user_id": frappe.session.user}, "name")
         
-        if employee:
-            # Build the query conditions based on what we have
+        if employee_name:
+            # Get the employee record with their primary attributes
+            employee = frappe.db.get_value("Employee", 
+                                         employee_name, 
+                                         ["custom_supplier", "branch", "custom_sub_branch"],
+                                         as_dict=True)
+            
+            # Get additional sub branches from the sub branch access list
+            additional_sub_branches = []
+            sub_branch_list = frappe.db.sql("""
+                SELECT sub_branch 
+                FROM `tabSub Branch Table` 
+                WHERE parent = %s
+            """, (employee_name,), as_dict=True)
+            
+            additional_sub_branches = [sb.sub_branch for sb in sub_branch_list if sb.sub_branch]
+            
+            # Build the query conditions
             conditions = []
             values = []
             
@@ -65,9 +81,23 @@ def get_data(filters):
                 conditions.append("custom_branch = %s")
                 values.append(employee.branch)
             
-            if employee.custom_sub_branch:
-                conditions.append("custom_sub_branch = %s")
-                values.append(employee.custom_sub_branch)
+            # Handle both primary sub branch and additional sub branches from the list
+            if employee.custom_sub_branch or additional_sub_branches:
+                # Start with an empty sub branch list
+                sub_branch_list = []
+                
+                # Add primary sub branch if it exists
+                if employee.custom_sub_branch:
+                    sub_branch_list.append(employee.custom_sub_branch)
+                
+                # Add additional sub branches from the list
+                sub_branch_list.extend(additional_sub_branches)
+                
+                # If we have any sub branches, add them to the condition
+                if sub_branch_list:
+                    placeholders = ", ".join(["%s"] * len(sub_branch_list))
+                    conditions.append(f"custom_sub_branch IN ({placeholders})")
+                    values.extend(sub_branch_list)
             
             if conditions:
                 condition_string = " AND ".join(conditions)
